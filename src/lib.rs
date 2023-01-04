8d2d9919d642def9b112069e95f13b09893702c9
marcus1337 <blurgas@hotmail.com>
2023-01-04 22:14:01 +0100

    change api names

--- src/lib.rs
+++ src/lib.rs
@@ -17,58 +17,58 @@ pub struct Gomoku {
 
 impl Gomoku {
     #[no_mangle]
-    pub extern "C" fn make() -> Self {
+    pub extern "C" fn gomoku_make() -> Self {
         Self {
             board: Board::new(),
         }
     }
 
     #[no_mangle]
-    pub extern "C" fn reset(&mut self) {
+    pub extern "C" fn gomoku_reset(&mut self) {
         self.board.reset();
     }
 
     #[no_mangle]
-    pub extern "C" fn get_ai_move(&self) -> Point {
+    pub extern "C" fn gomoku_get_ai_move(&self) -> Point {
         ai::get_placement_point(self.board)
     }
 
     #[no_mangle]
-    pub extern "C" fn print(&mut self) {
+    pub extern "C" fn gomoku_print(&mut self) {
         println!("{}", self.board);
     }
 
     #[no_mangle]
-    pub extern "C" fn get_result(&self) -> GameResult {
+    pub extern "C" fn gomoku_get_result(&self) -> GameResult {
         return self.board.get_result();
     }
 
     #[no_mangle]
-    pub extern "C" fn has_brick(&self, point: Point) -> bool {
+    pub extern "C" fn gomoku_has_brick(&self, point: Point) -> bool {
         self.board.has_brick(point)
     }
     #[no_mangle]
-    pub extern "C" fn get_brick(&self, point: Point) -> Brick {
+    pub extern "C" fn gomoku_get_brick(&self, point: Point) -> Brick {
         self.board.get_brick(point)
     }
 
     #[no_mangle]
-    pub extern "C" fn get_num_bricks(&self) -> i32 {
+    pub extern "C" fn gomoku_get_num_bricks(&self) -> i32 {
         self.board.get_num_bricks()
     }
 
     #[no_mangle]
-    pub extern "C" fn get_next_brick(&self) -> Brick {
+    pub extern "C" fn gomoku_get_next_brick(&self) -> Brick {
         self.board.get_next_brick()
     }
 
     #[no_mangle]
-    pub extern "C" fn place_brick(&mut self, point: Point) {
+    pub extern "C" fn gomoku_place_brick(&mut self, point: Point) {
         self.board.place_brick(point);
     }
 
     #[no_mangle]
-    pub extern "C" fn is_started(&self) -> bool {
+    pub extern "C" fn gomoku_is_started(&self) -> bool {
         self.board.has_any_bricks()
     }
 
